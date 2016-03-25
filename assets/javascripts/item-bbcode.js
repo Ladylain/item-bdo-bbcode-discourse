90462f5deba31d324e673cf12b80196abc35d2d1
Ladylain <ladylain73@gmail.com>
2016-03-25 17:28:06 +0100

    Delete item-bbcode.js

--- assets/javascripts/item-bbcode.js
+++
@@ -1,6 +0,0 @@
-// [CARD]...[/CARD]
-Discourse.BBCode.replaceBBCode('item', function(contents) { 
-  var url = contents;
-  return ['a', {'class': 'item-bbcode','href': "#", 'onmouseover' : "javascript:url_to_item('"+contents+"')");
-});
-

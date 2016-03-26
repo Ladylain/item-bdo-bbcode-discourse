1800208a26493ee65b83b6d61f04edd4ef919af7
Ladylain <ladylain73@gmail.com>
2016-03-26 04:00:56 +0100

    Added files via upload

--- assets/javascripts/item_bbcode.js
+++ assets/javascripts/item_bbcode.js
@@ -1,6 +1,13 @@
-// [CARD]...[/CARD]
-Discourse.BBCode.replaceBBCode('item', function(contents) { 
-  var url = contents;
-  return ['a', {'class': 'item-bbcode','href': "#", 'onmouseenter' : "javascript:url_to_item('"+contents+"')");
-});
 
+(function() {
+  function replaceItemLink (text) {
+    while (text != (text = text.replace(/\[item=([^\]]+)\]((?:(?!\[item=[^\]]+\]|\[\/item\])[\S\s])*)\[\/item\]/ig, function (match, p1, p2, offset, string) {
+      return "<a href=\"#\" onmouseenter=\"javascript:url_to_item('"+p1+"')\" class=\"item-bdo\">" + p2 + "</a>";
+    })));
+    return text;
+  }
+
+  Discourse.Dialect.addPreProcessor(replaceItemLink);
+  Discourse.Markdown.whiteListTag('a', 'href', 'onmouseenter', 'class');
+
+})();

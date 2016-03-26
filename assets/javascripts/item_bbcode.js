38225dcc557412577e6f03f9e620d2ef6d21c2ee
Ladylain <ladylain73@gmail.com>
2016-03-26 16:21:10 +0100

    Added files via upload

--- assets/javascripts/item_bbcode.js
+++ assets/javascripts/item_bbcode.js
@@ -2,11 +2,32 @@
 (function() {
   function replaceItemLink (text) {
     while (text != (text = text.replace(/\[item=([^\]]+)\]((?:(?!\[item=[^\]]+\]|\[\/item\])[\S\s])*)\[\/item\]/ig, function (match, p1, p2, offset, string) {
-      return "<a href="+p1+" class=\"item-bdo\">" + p2 + "</a>";
+      var url_img; 
+      url_img = $.ajax({
+    		   url: "http://digitalsky.fr/aelon/get_content.php",
+    		   type: "GET",
+    		   cache: false,
+    		   dataType: 'jsonp',
+    		   data: {url: url},
+    		   success: function(reponse){
+    		   		/*reponse = jQuery.parseJSON(reponse);
+    			   	console.log(reponse);  	*/
+    			   	reponse = reponse.split('<div class="outer item_info">');
+    			   	reponse = reponse[1].split('</div>');
+    			   	reponse = reponse[0];
+    			   	urlImage = reponse.split('<td class="icon_cell"><img src="');
+    			   	console.log(urlImage);
+    			   	urlImage = urlImage[1].split('" class="item_icon" alt="icon"></td>');
+    			   	console.log(urlImage);
+    			   	urlImageFinal = urlImage[0];
+    			   	console.log(urlImageFinal);
+    			   	return urlImageFinal;
+    			}
+    	});
+      return "<a href=\"http://bddatabase.net/fr/item/"+p1+"\" class=\"item-bdo\"><img src=\"http://bddatabase.net/" + url_img + "\"" + p2 + "</img></a>";
     })));
     return text;
   }
-
   Discourse.Dialect.addPreProcessor(replaceItemLink);
   Discourse.Markdown.whiteListTag('a', 'class', 'item-bdo');
 

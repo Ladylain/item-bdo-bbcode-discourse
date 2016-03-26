41ae1b981624ba5303b28c3815cb1d47958716e7
Ladylain <ladylain73@gmail.com>
2016-03-26 13:27:37 +0100

    Added files via upload

--- assets/javascripts/item_bbcode.js
+++ assets/javascripts/item_bbcode.js
@@ -2,12 +2,12 @@
 (function() {
   function replaceItemLink (text) {
     while (text != (text = text.replace(/\[item=([^\]]+)\]((?:(?!\[item=[^\]]+\]|\[\/item\])[\S\s])*)\[\/item\]/ig, function (match, p1, p2, offset, string) {
-      return "<a href=\"#\" onmouseenter=\"javascript:url_to_item('"+p1+"')\" class=\"item-bdo\">" + p2 + "</a>";
+      return "<a href="+p1+" class=\"item-bdo\">" + p2 + "</a>";
     })));
     return text;
   }
 
   Discourse.Dialect.addPreProcessor(replaceItemLink);
-  Discourse.Markdown.whiteListTag('a', 'href', 'onmouseenter', 'class');
+  Discourse.Markdown.whiteListTag('a', 'class', 'item-bdo');
 
 })();

0e2d9e0f9960c07d20281de3573332cd19ccca17
Ladylain <ladylain73@gmail.com>
2016-03-26 03:26:12 +0100

    Added files via upload

--- assets/javascripts/item_bbcode.js
+++ assets/javascripts/item_bbcode.js
@@ -32,13 +32,12 @@
 }
   function replaceItemLink (text) {
     while (text != (text = text.replace(/\[item=([^\]]+)\]((?:(?!\[item=[^\]]+\]|\[\/item\])[\S\s])*)\[\/item\]/ig, function (match, p1, p2, offset, string) {
-      return "<a href="+p1+" class=\"item-bdo\">" + p2 + "</a>";
+      return "<a href=\"#\" onmouseenter=\"javascript:url_to_item('"+p1+"')\" class=\"item-bdo\">" + p2 + "</a>";
     })));
     return text;
   }
 
   Discourse.Dialect.addPreProcessor(replaceItemLink);
-  Discourse.Markdown.whiteListTag('a', 'href');
-  Discourse.Markdown.whiteListTag('a', 'class', 'item-bdo');
+  Discourse.Markdown.whiteListTag('a', 'href', '*',  'onmouseenter', '*', 'class', 'item-bdo');
 
 })();
